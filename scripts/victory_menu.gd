d9b2214231b53a9ebbd8d17b034e3bb522560239
ChristianumSolacium <73193971+ChristianumSolacium@users.noreply.github.com>
2025-02-02 13:03:58 +0100

    fix post merge

--- scripts/victory_menu.gd
+++ scripts/victory_menu.gd
@@ -1,1 +1,11 @@
 extends Control
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_replay_button_pressed() -> void:
+	#TODO Aggiungere funzione replay
+	pass # Replace with function body.
+	
