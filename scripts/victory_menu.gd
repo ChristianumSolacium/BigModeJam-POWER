c727fd89ea1bfada2548a86b828d3677bdda51d8
ChristianumSolacium <73193971+ChristianumSolacium@users.noreply.github.com>
2025-02-02 16:31:28 +0100

    victory menu

--- scripts/victory_menu.gd
+++ scripts/victory_menu.gd
@@ -4,19 +4,25 @@ signal restart
 @onready var score_label: Label = $ScoreLabel
 
 func _on_quit_button_pressed() -> void:
+	$HBoxContainer/QuitButton/Button3Animations.show()
 	get_tree().quit()
+	
 
 
 func _on_replay_button_pressed() -> void:
 	#TODO Aggiungere funzione replay
 	pass # Replace with function body.
 	
+	
 func _on_continue_button_pressed() -> void:
+	$HBoxContainer/ContinueButton/Button4Animations.show()
 	continue_next_level.emit()
+	
 
 
 func _on_restart_button_pressed() -> void:
+	$HBoxContainer/RestartButton/Button1Animations.show()
 	restart.emit()
-
+	
 func set_score(value:int) -> void:
 	score_label.text = "SCORE: " + str(value)
