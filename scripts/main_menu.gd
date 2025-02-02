85bc3c1d15d5d363b44256ab87f354603136ae86
mt0t <55357222+mt0t@users.noreply.github.com>
2025-02-02 16:13:31 +0100

    tutorial

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var is_play_pressed = false
+
 func _ready() -> void:
 	$PolarityLoopMenu.play()
 	$CanvasLayer/MarginContainer/AnimatedSprite2D.play("titolo")
@@ -12,12 +14,21 @@ func _ready() -> void:
 	ResourceSaver.save(config,CONFIG_PATH)
 
 func _on_play_pressed() -> void:
+	is_play_pressed = true
 	$PolaritySdPress.play()
-	get_tree().change_scene_to_file("res://scenes/logica interna/main.tscn")
+	$Tutorial.show()
+	$CanvasLayer.hide()
+	if Input.is_action_just_pressed(&"jump"):
+		get_tree().change_scene_to_file("res://scenes/logica interna/main.tscn")
 	
-
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+	
+func _process(delta: float) -> void:
+	if is_play_pressed:
+		if Input.is_action_just_pressed(&"jump"):
+			get_tree().change_scene_to_file("res://scenes/logica interna/main.tscn")
+	
